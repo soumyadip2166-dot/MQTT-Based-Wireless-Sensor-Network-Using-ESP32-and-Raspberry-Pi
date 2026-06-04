d3f6d183954434d6b58c913643ff68976657286f
Dr. Soumyadip Banerjee <soumyadip2166@gmail.com>
2026-06-04 14:17:40 +0530

    Update dht11_lcd16x2MQTT.ino

--- dht11_lcd16x2MQTT.ino
+++ dht11_lcd16x2MQTT.ino
@@ -1,3 +1,5 @@
+#WSN - 3
+
 #include <ESP8266WiFi.h>
 #include <MQTT.h>
 #include <SPI.h>
@@ -14,8 +16,8 @@
 WiFiClient net;
 MQTTClient client;
 
-String ssid="ProjectWifi";
-String pass="237a27d13B";
+String ssid="SSID";
+String pass="PASSWORD";
 unsigned long lastMillis = 0;
  
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // I2C address 0x27, 16 column and 2 rows
@@ -44,7 +46,7 @@ void setup() {
   lcd.init();         // initialize the lcd
   lcd.backlight();    // open the backlight
   WiFi.begin(ssid, pass);
-  client.begin("192.168.1.102",net);
+  client.begin("IP Address of the MQTT Server",net);
   client.onMessage(messageReceived);
   connect();
   delay(10);
